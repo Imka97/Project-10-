8ea267c7a299ee374d92f1b396c6979ae5ed0418
Цомаева Иман <85078977+Imka97@users.noreply.github.com>
2023-03-29 13:58:20 +0300

    Добавили ярлык

--- My Project 10/My Project 10/ViewController.swift
+++ My Project 10/My Project 10/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var label: UILabel!
+    
+    override func loadView() {
+        label = UILabel() 
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
