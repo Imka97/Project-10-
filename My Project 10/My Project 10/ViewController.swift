17f7356c42f134c58404bce7056d35e3fe07602f
Цомаева Иман <85078977+Imka97@users.noreply.github.com>
2023-03-29 13:57:27 +0300

    init Project

---
+++ My Project 10/My Project 10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Project 10
+//
+//  Created by Imka on 29.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
